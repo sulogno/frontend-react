9d54806aaddccbb9adf0182e6a03320357cee42f
Sulogno Sarkar <rhitsarkar2003@gmail.com>
2025-08-03 19:34:02 +0530

    Card comonent

---
+++ frontend 2/src/App.jsx
@@ -0,0 +1,29 @@
+import Card from './card.jsx'
+
+
+
+function App(){
+    return(
+        <>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+
+
+
+        </>
+
+    );
+}
+
+export default App
---
+++ frontend 2/src/card.jsx
@@ -0,0 +1,15 @@
+
+
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image" src="https://i.pinimg.com/originals/40/71/d6/4071d667fae30cd0e003c165f9dc757e.png" alt="Image"></img>
+            <h2 className="card-title">Rhit</h2>
+            <p>Rhit is the top notch programmer</p>
+
+        </div>
+    );
+}
+
+
+export default Card
